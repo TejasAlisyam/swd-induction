66690dfdffb3abf98595a472164c0aae44ba4748
tejasalisyam <tejas.alisyam09@gmail.com>
2022-07-07 18:48:11 +0530

    about page added

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
 
- const About = () => {
+const About = () => {
   return (
     <div className="container">
-    <div className= "py-4">
-    <h1>About Page</h1>
+      <div className="py-4">
+        <h1>About Page</h1>
+        <p>
+          {" "}
+          I'm adept with coding, web development, real time problem solving and
+          teamwork. I expertise in translating project objectives into reality,
+          offering tangible results with required expectations. I play with
+          badminton regularly. Learing guitar from past 1 month
+        </p>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
 export default About;
